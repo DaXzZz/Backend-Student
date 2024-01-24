50a45afcddaf448499a58cfe08346fb63cab3c09
Nontapat Chucharnchai <ryurock1011@gmail.com>
2024-01-24 18:33:36 +0700

    Add-Greet-Get(All)-Get(Id)

--- student.py
+++ student.py
@@ -13,5 +13,24 @@ client = MongoClient("mongodb+srv://mongo:mongo@cluster0.bll8kuf.mongodb.net/?re
 db = client["students"]
 students_collection = db["std_info"]
 
+@app.route("/")
+def greet():
+    return "Welcome to Student Management API"
+
+@app.route("/students", methods=["GET"])
+@basic_auth.required
+def get_all_students():
+    students = students_collection.find()
+    return jsonify([student for student in students])
+
+@app.route("/students/<string:_id>", methods=["GET"])
+@basic_auth.required
+def get_student(_id):
+    student = students_collection.find_one({'_id': _id})
+    if student:
+        return jsonify(student)
+    else:
+        return jsonify({"error":"Student not found"}), 404
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
